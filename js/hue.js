50fc324f96a74066f5c5b21cf49904f50b527a7f
Richa Mehta <purplestencil@gmail.com>
2017-01-30 20:55:44 -0500

    Add huebrew.html and hue.js

---
+++ js/hue.js
@@ -0,0 +1,12 @@
+/*JavaScript code for Project Huebrew
+  Author: Richa Mehta
+*/
+
+//User name included in the whitelist
+brew.user = 'whitelisteduser';
+
+//Bridge IP address
+brew.ipAdd =  null;
+
+
+
